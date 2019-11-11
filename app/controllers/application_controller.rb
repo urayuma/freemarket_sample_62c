8188b19935c44707f14011b1277245418f224201
深草祐人 <fukahiko@gmail.com>
2019-11-11 21:44:07 +0900

    追加、 deviseのストロングパラメータ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,5 +12,22 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:lastname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:lastname_kana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname_kana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:birthday_year])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:birthday_month])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:birthday_day])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:phonenumber])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:introduction])
   end
 end
