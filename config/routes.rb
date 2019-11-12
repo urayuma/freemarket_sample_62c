1b6513afc7fda672f5457c384819a67854152557
MomokaIgarashi <migarashi0318@gmail.com>
2019-11-12 23:13:34 +0900

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
-<<<<<<< HEAD
   get   'home/index', to: 'home#index'
   get   'mypage/index', to: 'mypage#index'
   get   'mypage/edit', to: 'mypage#edit'
@@ -15,8 +14,5 @@ Rails.application.routes.draw do
       get 'done' # 登録完了後のページ
     end
   end
-=======
-  get  'home/index'  =>  'home#index'
->>>>>>> 29678d912e3d7f6d9cf6a9761a3b3a8ad2fe203f
 
 end
