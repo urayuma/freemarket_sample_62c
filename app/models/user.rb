ec3e01550866628d100f90b4290f39e04826f381
深草祐人 <fukahiko@gmail.com>
2019-11-12 19:41:21 +0900

    Addressモデルにバリデーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ApplicationRecord
   validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{7,128}+\z/i } #英字と数字の両方を含む7文字以上128文字以下
   validates :lastname, presence: true, length:{ maximum: 35 }
   validates :firstname, presence: true, length:{ maximum: 35 }
-  validates :lastname_kana, presence: true, length:{ maximum: 35 }, format: {with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/} #カタカナのみ
-  validates :firstname_kana, presence: true, length:{ maximum: 35 }, format: {with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/}
+  validates :lastname_kana, presence: true, length:{ maximum: 35 }, format: {with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/} #カタカナのみ
+  validates :firstname_kana, presence: true, length:{ maximum: 35 }, format: {with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/}
   validates :birthday_year, presence: true, length:{ maximum: 35 }, numericality: { only_integer: true, greater_than: 1899, less_than: 2020 } #1900以上2019以下の整数
   validates :birthday_month, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 13 } #1以上12以下の整数
   validates :birthday_day, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 32 } #1以上31以下の整数
@@ -18,4 +18,3 @@ class User < ApplicationRecord
   validates :introduction, length:{ maximum: 1000 }
 
 end
-
