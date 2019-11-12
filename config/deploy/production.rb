841b9283edac01cb37fb53a860dcafda287ca491
Kotaro_Furuich <happy1998k@gmai.com>
2019-11-12 17:19:11 +0900

    basic認証を設定

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -50,3 +50,6 @@
 # The server-based syntax can be used to override options:
 # ------------------------------------
 server "18.177.15.224", user: "ec2-user", roles: %w{app db web}
+
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
