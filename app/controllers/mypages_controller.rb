597a5269d5480358f5b555fd47b44f065ad7eac1
Kotaro_Furuich <happy1998k@gmai.com>
2019-11-12 15:27:10 +0900

    Routes revise

---
+++ app/controllers/mypages_controller.rb
@@ -0,0 +1,10 @@
+class MypagesController < ApplicationController
+  def index
+  end
+
+  def edit
+  end
+
+  def card
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  root 'homes#index'
+  get 'home/index', to: 'homes#index'
+  get 'mypage/index', to: 'mypages#index'
+  get 'mypage/edit', to: 'mypages#index'
+  get 'mypage/card', to: 'mypages#index'
+  
+  resources :signups do
+    collection do
+      get 'step1'
+      get 'step2'
+      get 'step3'
+      get 'step4' # ここで、入力の全てが終了する
+      get 'done' # 登録完了後のページ
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
