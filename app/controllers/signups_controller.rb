cebc196b8632a6dc472184fc1043962f12a6b0b1
Kotaro_Furuich <happy1998k@gmai.com>
2019-11-13 15:51:30 +0900

    ユーザ登録のコントロール部分完成

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,16 +1,78 @@
 class SignupsController < ApplicationController
   def step1
+    @user = User.new
   end
 
   def step2
+    session[:nickname] = user_params[:nickname]
+    session[:email] = user_params[:email]
+    session[:password] = user_params[:password]
+    session[:lastname] = user_params[:lastname]
+    session[:firstname] = user_params[:firstname]
+    session[:lastname_kana] = user_params[:lastname_kana]
+    session[:firstname_kana] = user_params[:firstname_kana]
+    session[:birthday_year] = user_params[:birthday_year]
+    session[:birthday_month] = user_params[:birthday_month]
+    session[:birthday_day] = user_params[:birthday_day]
+    @user = User.new
   end
 
-  def step3
+  def creata
+    @user = User.new(
+      nickname: session[:nickname],
+      email: session[:email],
+      password: session[:password],
+      lastname: session[:lastname],
+      firstname: session[:firstname],
+      lastname_kana: session[:lastname_kana],
+      firstname_kana: session[:firstname_kana],
+      birthday_year: session[:birthday_year],
+      birthday_month: session[:birthday_month],
+      birthday_day: session[:birthday_day],
+      phonenumber: user_params[:phonenumber]
+    )
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to new_address_path
+    else
+      render '/signup/step2'
+    end
   end
 
-  def step4
-  end
+  private
 
-  def done
+    def user_params
+      params.require(:user).permit(:nickname, :email, :password, :lastname, :firstname,
+                                    :lastname_kana, :firstname_kana, :birthday_year,
+                                    :birthday_month, :birthday_day, :phonenumber)
+    end
+
+    def validates_step1
+      session[:nickname] = user_params[:nickname]
+      session[:email] = user_params[:email]
+      session[:password] = user_params[:password]
+      session[:lastname] = user_params[:lastname]
+      session[:firstname] = user_params[:firstname]
+      session[:lastname_kana] = user_params[:lastname_kana]
+      session[:firstname_kana] = user_params[:firstname_kana]
+      session[:birthday_year] = user_params[:birthday_year]
+      session[:birthday_month] = user_params[:birthday_month]
+      session[:birthday_day] = user_params[:birthday_day]
+      
+      @user = User.new(
+        nickname: session[:nickname],
+        email: session[:email],
+        password: session[:password],
+        lastname: session[:lastname],
+        firstname: session[:firstname],
+        lastname_kana: session[:lastname_kana],
+        firstname_kana: session[:firstname_kana],
+        birthday_year: session[:birthday_year],
+        birthday_month: session[:birthday_month],
+        birthday_day: session[:birthday_day],
+        phonenumber: 0000000000
+      )
+      render '/signup/step1' unless @user.valid?
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'homes#index'
   get 'home/index', to: 'homes#index'
   get 'mypage/index', to: 'mypages#index'
   get 'mypage/edit', to: 'mypages#index'
   get 'mypage/card', to: 'mypages#index'
   
-  resources :signups do
+  resources :signups, only: [:create] do
     collection do
       get 'step1'
       get 'step2'
-      get 'step3'
-      get 'step4' # ここで、入力の全てが終了する
-      get 'done' # 登録完了後のページ
     end
   end
-
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
