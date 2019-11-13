9aa6f6d047986b19a30c24717f7bf977f5ea13ff
深草祐人 <fukahiko@gmail.com>
2019-11-13 15:55:04 +0900

    Userテストコード記述、firstnameまで

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true, length:{ maximum: 20 }
   validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{7,128}+\z/i } #英字と数字の両方を含む7文字以上128文字以下
-  validates :lastname, presence: true, length:{ maximum: 35 }
-  validates :firstname, presence: true, length:{ maximum: 35 }
+  validates :lastname, presence: true, length:{ maximum: 35 }, format: { with:/\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々]|[a-zA-Z]|[ｧ-ﾝﾞﾟ]|[ａ-ｚＡ-Ｚ])+\z/i} #全角ひらがな、全角カタカナ、漢字、半角英字、半角カタカナ、全角英字
+  validates :firstname, presence: true, length:{ maximum: 35 }, format: { with:/\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々]|[a-zA-Z]|[ｧ-ﾝﾞﾟ]|[ａ-ｚＡ-Ｚ])+\z/i} #全角ひらがな、全角カタカナ、漢字、半角英字、半角カタカナ、全角英字
   validates :lastname_kana, presence: true, length:{ maximum: 35 }, format: {with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/} #カタカナのみ
   validates :firstname_kana, presence: true, length:{ maximum: 35 }, format: {with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/}
   validates :birthday_year, presence: true, length:{ maximum: 35 }, numericality: { only_integer: true, greater_than: 1899, less_than: 2020 } #1900以上2019以下の整数
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 describe User do
   describe '#create' do
 
+    it "全ての値が適切に入力されている場合、登録できること" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
     it "emailが空の場合、登録できないこと" do
       user = build(:user, email: "")
       user.valid?
@@ -13,11 +18,49 @@ describe User do
       user.valid?
       expect(user.errors[:email]).to include("は不正な値です")
     end
+    
+    it "emailの中間に@がある場合、登録できること" do
+      user = build(:user, email: "email@email")
+      user.valid?
+      expect(user).to be_valid
+    end
 
     it "passwordが空の場合、登録できないこと" do
       user = build(:user, password: "")
       user.valid?
       expect(user.errors[:password]).to include("を入力してください")
+    end
+
+    it "passwordが6文字以下の場合、登録できないこと" do
+      user = build(:user, password: "12345q")
+      user.valid?
+      expect(user.errors[:password]).to include("は7文字以上で入力してください", "は不正な値です")
+    end
+
+    it "passwordが129文字以上の場合、登録できないこと" do
+      o = [('a'..'z'),('A'..'Z'),('0'..'9')].map{|i| i.to_a}.flatten
+      password = (0...129).map { o[rand(o.length)] }.join
+      user = build(:user, password: "#{password}")
+      user.valid?
+      expect(user.errors[:password]).to include("は128文字以内で入力してください")
+    end
+
+    it "passwordが数字のみの場合、登録できないこと" do
+      user = build(:user, password: "1234567")
+      user.valid?
+      expect(user.errors[:password]).to include("は不正な値です")
+    end
+
+    it "passwordが英字のみの場合、登録できないこと" do
+      user = build(:user, password: "abcdefg")
+      user.valid?
+      expect(user.errors[:password]).to include("は不正な値です")
+    end
+
+    it "passwordが数字と英字を含む7文字以上128文字以下の場合、登録できること" do
+      user = build(:user, password: "1a2b3c4")
+      user.valid?
+      expect(user).to be_valid
     end
 
     it "nicknameが空の場合、登録できないこと" do
@@ -27,11 +70,89 @@ describe User do
     end
 
     it "nicknameが21文字以上の場合、登録できないこと" do
-      name = (0...21).map{ ('A'..'Z').to_a[rand(26)] }.join #英字で21文字の文字列を生成
-      user = build(:user, nickname: "#{name}")
+      nickname = (0...21).map{ ('A'..'Z').to_a[rand(26)] }.join #英字で21文字の文字列を生成
+      user = build(:user, nickname: "#{nickname}")
       user.valid?
       expect(user.errors[:nickname]).to include("は20文字以内で入力してください")
     end
+
+    it "nicknameが20文字以内の場合、登録できること" do
+      nickname = (0...20).map{ ('A'..'Z').to_a[rand(26)] }.join #英字で20文字の文字列を生成
+      user = build(:user, nickname: "#{nickname}")
+      user.valid?
+      expect(user).to be_valid
+    end
+
+    it "lastnameが空の場合、登録できないこと" do
+      user = build(:user, lastname: "")
+      user.valid?
+      expect(user.errors[:lastname]).to include("を入力してください")
+    end
+
+    it "lastnameが36文字以上の場合、登録できないこと" do
+      lastname = (0...36).map{ ('A'..'Z').to_a[rand(26)] }.join #英字で36文字の文字列を生成
+      user = build(:user, lastname: "#{lastname}")
+      user.valid?
+      expect(user.errors[:lastname]).to include("は35文字以内で入力してください")
+    end
+
+    it "lastnameが特殊文字を含む場合、登録できないこと" do
+      user = build(:user, lastname: "@＠!！_?><")
+      user.valid?
+      expect(user.errors[:lastname]).to include("は不正な値です")
+    end
+
+    it "lastnameが数字を含む場合、登録できないこと" do
+      user = build(:user, lastname: "123１２３")
+      user.valid?
+      expect(user.errors[:lastname]).to include("は不正な値です")
+    end
+
+    it "lastnameが数字や特殊文字を含まず35文字以内の場合、登録できること" do
+      lastname = (0...35).map{ ('A'..'Z').to_a[rand(26)] }.join #英字で35文字の文字列を生成
+      user = build(:user, lastname: "#{lastname}")
+      user.valid?
+      expect(user).to be_valid
+    end
+
+    it "firstnameが空の場合、登録できないこと" do
+      user = build(:user, firstname: "")
+      user.valid?
+      expect(user.errors[:firstname]).to include("を入力してください")
+    end
+    
+    it "firstnameが36文字以上の場合、登録できないこと" do
+      firstname = (0...36).map{ ('A'..'Z').to_a[rand(26)] }.join #英字で36文字の文字列を生成
+      user = build(:user, firstname: "#{firstname}")
+      user.valid?
+      expect(user.errors[:firstname]).to include("は35文字以内で入力してください")
+    end
+    
+    it "firstnameが特殊文字を含む場合、登録できないこと" do
+      user = build(:user, firstname: "@＠!！_?><")
+      user.valid?
+      expect(user.errors[:firstname]).to include("は不正な値です")
+    end
+    
+    it "firstnameが数字を含む場合、登録できないこと" do
+      user = build(:user, firstname: "123１２３")
+      user.valid?
+      expect(user.errors[:firstname]).to include("は不正な値です")
+    end
+    
+    it "firstnameが数字や特殊文字を含まず35文字以内の場合、登録できること" do
+      firstname = (0...35).map{ ('A'..'Z').to_a[rand(26)] }.join #英字で35文字の文字列を生成
+      user = build(:user, firstname: "#{firstname}")
+      user.valid?
+      expect(user).to be_valid
+    end
+
+
+
+
+
+
+
 
   end
 end
