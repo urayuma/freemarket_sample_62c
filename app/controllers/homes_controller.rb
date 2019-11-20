8e187957a06c5dad44fcf8662cb12c95c4f6a299
MomokaIgarashi <migarashi0318@gmail.com>
2019-11-20 15:21:13 +0900

    Faker記述

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,3 +1,24 @@
 class HomesController < ApplicationController
-  def index; end
+  def index
+    @like_rankings = Image.find(Like.group(:item_id).order('count(item_id) desc').limit(4).pluck(:item_id))
+
+    @category_rankings = Category.find(Item.group(:category_id).order('count(category_id) desc').limit(4).pluck(:category_id))
+
+    @categorytitle = Category.find(Item.group(:category_id).order('count(category_id) desc').limit(1).pluck(:category_id))
+
+    @categoryitems = Item.where(category_id: @categorytitle[0].id)
+
+    @brand_rankings = Brand.find(Item.group(:brand_id).order('count(brand_id) desc').limit(4).pluck(:brand_id))
+
+    @brandtitle = Brand.find(Item.group(:brand_id).order('count(brand_id) desc').limit(1).pluck(:brand_id))
+
+    @branditems = Item.where(brand_id: @brandtitle[0].id)
+  end
+
+  private
+  def item_params
+    params.permit(:email, :password)
+  end
+
+
 end
