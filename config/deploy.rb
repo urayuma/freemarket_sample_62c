3f95d7edf46d923a9281fbb6f0a52b3c1db9ddd1
Kotaro_Furuich <happy1998k@gmai.com>
2019-11-27 12:04:30 +0900

    correct capコマンドでデプロイ時に環境変数が読み込まれない問題を解決

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,13 +31,13 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-set :default_env,
-    rbenv_root: "/usr/local/rbenv",
-    path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-    BASIC_AUTH_USER: ENV["BASIC_AUTH_USER"],
-    BASIC_AUTH_PASSWORD: ENV["BASIC_AUTH_PASSWORD"],
-    AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-    AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+# set :default_env,
+#     rbenv_root: "/usr/local/rbenv",
+#     path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+#     BASIC_AUTH_USER: ENV["BASIC_AUTH_USER"],
+#     BASIC_AUTH_PASSWORD: ENV["BASIC_AUTH_PASSWORD"],
+#     AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+#     AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
 
 set :linked_files, %w[config/secrets.yml]
 
@@ -50,8 +50,10 @@ namespace :deploy do
 
   desc 'upload secrets.yml'
   task :upload do
-    on roles(:app) do |_host|
-      execute "mkdir -p #{shared_path}/config" if test "[ ! -d #{shared_path}/config ]"
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
       upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
     end
   end
