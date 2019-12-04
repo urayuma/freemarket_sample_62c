dc757384ed3d9cc83f26e79936724926f9e9e68e
toto13-git <56705907+toto13-git@users.noreply.github.com>
2019-12-04 13:21:54 +0900

    itemのvalidatesです

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,3 +8,18 @@ class Item < ApplicationRecord
   belongs_to :category, optional: true
   accepts_nested_attributes_for :images
 end
+
+  validates :user_id, presence: true, uniqueness: true
+  validates :image, presence: true
+  validates :name, presence: true, inclusion: { in: 1..40, message: "入力してください" }
+  validates :description, presence: true, inclusion: { in: 1..1000, message: "入力してください" }
+  validates :category_id, presence: true
+  validates :size, presence: true
+  validates :usage_status, presence: true
+  validates :delivery_fee, presence: true
+  validates :delivery_way, presence: true
+  validates :delivery_area, presence: true
+  validates :shipping_date, presence: true
+  validates :price, presence: true, inclusion: { in: 300..9999999, message: "入力してください"}, numericality: { only_integer: true }
+
+end
