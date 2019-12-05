defbb988cb856edd2c15733d26bf37bb5d8434be
深草祐人 <fukahiko@gmail.com>
2019-12-05 17:23:58 +0900

    編集後リダイレクト先変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -80,14 +80,17 @@ class ItemsController < ApplicationController
   def show; end
 
   def update
+
     @item = Item.find(params[:id])
     if @item.update(item_params)
       Image.where(item: @item).delete_all
       params[:images][:image].each do |image|
         @item.images.create(image: image, item_id: @item.id)
       end
+      redirect_to item_show_mypage_index_path(params[:id])
+    else
+      redirect_to edit_item_path(params[:id])
     end
-    redirect_to edit_item_path(params[:id])
   end
 
   def destroy
