66b8c9fd92682d82f3b1e81f3be7ed75a264eba3
furuichi <happy1998k@gmai.com>
2019-12-05 15:21:49 +0900

    IAMユーザとS３を再度作成して設定を変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,12 +8,12 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
       region: 'ap-northeast-1'
     }
-    config.fog_directory = 'mercri-s3'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/mercri-s3'
+    config.fog_directory = 'freemarketsample62c-s3'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarketsample62c-s3'
   else
     config.storage :file # 開発環境:public/uploades下に保存
     config.enable_processing = false if Rails.env.test? #test:処理をスキップ
