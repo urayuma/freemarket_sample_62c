2f82b43a943a7194805c743307a29087a7661f93
深草祐人 <fukahiko@gmail.com>
2019-12-05 13:46:43 +0900

    サイドバー修正

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -72,7 +72,7 @@ class MypagesController < ApplicationController
 
   def sidebar_setting
     @menulist = ["マイページ", "お知らせ", "やることリスト", "いいね！一覧", "出品する", "出品した商品 - 出品中", "出品した商品 - 取引中", "出品した商品 - 売却済み", "購入した商品 - 取引中", "購入した商品 - 過去の取引", "ニュース一覧", "評価一覧", "ガイド", "お問い合わせ"]
-    @linklist = ["/mypage", "/info/", "/mustdo/", "/lieks_all/", "/items/sell", "/mypage/listings/listing", "/mypage/listings/in_progress", "/mypage/listings/completed", "/mypage/purchase", "/mypage/purchased"]
+    @linklist = ["/mypage", "/mypage", "/mypage", "/mypage", "/items/sell", "/mypage/listings/listing", "/mypage/listings/in_progress", "/mypage/listings/completed", "/mypage/purchase", "/mypage/purchased"]
     @configlist = ["プロフィール", "発送元・お届け先住所変更", "支払い方法", "メール/パスワード", "本人情報", "電話番号の確認", "ログアウト"]
     @configlinklist = ["/mypage/edit", "/mypage/address", "/mypage/card", "/mypage/", "/mypage/identification", "/mypage/", "/mypage/logout/"]
   end
