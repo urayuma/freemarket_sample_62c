18fdd7b79d65ceda704e29ae9b57c2b27cb73e49
MomokaIgarashi <migarashi0318@gmail.com>
2019-12-05 21:51:23 +0900

    トップページいいね表示修正

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,17 +1,11 @@
-document.addEventListener(
-  "DOMContentLoaded",
-  e => {
-    let modal_open = document.getElementById("modal-open-btn");
-    modal_open.onclick = function() {
-      $("#overlay").fadeIn();
-      document.getElementById("modal-close-btn").onclick = function() {
-        $("#overlay").fadeOut();
-      };
-      document.getElementById("delete-comformation-btn").onclick = function() {
-        console.log("fejfdfa");
-        document.getElementById("item-delete-btn").click();
-      };
-    };
-  },
-  false
-);
+$(document).on("turbolinks:load", function() {
+  $("#modal-open-btn").on("click", function() {
+    $("#overlay").fadeIn();
+  });
+  $("#modal-close-btn").on("click", function() {
+    $("#overlay").fadeOut();
+  });
+  $("#delete-comformation-btn").on("click", function() {
+    $("#item-delete-btn").trigger("click");
+  });
+});
