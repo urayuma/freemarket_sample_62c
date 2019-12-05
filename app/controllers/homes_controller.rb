2195e39f5e8c29d10e18a077592a295b6c4681c3
MomokaIgarashi <migarashi0318@gmail.com>
2019-12-05 13:16:33 +0900

    いいね機能アップデート

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,7 +4,9 @@ class HomesController < ApplicationController
   def index
     @items = Item.all
 
-    @like_rankings = Image.find(Like.group(:item_id).order('count(item_id) desc').limit(4).pluck(:item_id)) if user_signed_in? && Like.where(user_id: current_user.id).any?
+    # @like_rankings = Image.find(Like.group(:item_id).order('count(item_id) desc').limit(4).pluck(:item_id)) if user_signed_in? && Like.where(user_id: current_user.id).any?
+    # @like_rankings = Like.find(Like.group(:user_id).order('count(item_id) desc').limit(5).pluck(:user_id)) if user_signed_in? && Like.where(user_id: current_user.id).any?
+
 
     root_ids = Item.all.map { |item| item.category.root_id }
     sort_many = root_ids.instance_eval { uniq.sort_by { |x| count x }.reverse }
